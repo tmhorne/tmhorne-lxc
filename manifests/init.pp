c5f087ad67dd097ca6a9811cc3ba0adbf5c52654
Matt Horne <tmhorne@gmail.com>
2013-07-22 16:11:49 -0500

    Renamed README to README.md. Commented out my symlink to /home/lxc. Might be bothersome to others who fork or clone my repo.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,12 +2,15 @@ class lxc {
 
   package { 'lxc': ensure => installed }
   package { 'lxc-templates': ensure => installed }
-  
-  file { '/var/lib/lxc':
-    ensure	=> 'link',
-    target	=> '/home/lxc',
-    force	=> 'true',
-  }
+
+# I have my lxc machines in /home/lxc, so that I can rebuild my OS
+#   and as soon as I clone my private puppet repo and run puppet apply,
+#   everything works just like it did before I formatted & reinstalled.  
+#  file { '/var/lib/lxc':
+#    ensure	=> 'link',
+#    target	=> '/home/lxc',
+#    force	=> 'true',
+#  }
 
   exec { 'lxc-dnsmasq-setup':
     command	=> "lxc-dnsmasq-setup",
